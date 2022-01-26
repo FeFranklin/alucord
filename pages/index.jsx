01909b63340c8792ab0f39df42b7262eeabd8567
Felipe Franklin <felipelfbezerra54@gmail.com>
2022-01-26 21:02:42 +0100

    feat: tailwind refactor

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,8 @@ import appConfig from '../config.json';
 import GlobalStyle from '../components/GlobalStyle';
 import Title from '../components/Title';
 
+import style from '../components/style';
+
 const HomePage = () => {
   const username = 'peas';
 
@@ -15,26 +17,18 @@ const HomePage = () => {
     <>
       <GlobalStyle />
       <Box 
-        className={`flex items-center justify-center bg-[${appConfig.theme.colors.primary[500]}] bg-blend-multiply m-0 container bg-cover bg-no-repeat bg-[url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)]`}
+        className={`flex items-center justify-center ${style('bg', appConfig.theme.colors.primary[500])} bg-blend-multiply m-0 container bg-cover bg-no-repeat bg-[url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)]`}
       >
         <Box
-          className={`p-[32px] shadow-md m-4 flex xs:flex-column sm:flex-row items-center justify-between w-full max-w-[700px] rounded-2xl bg-[#212931]`}
+          className={`p-[32px] shadow-md m-4 flex xs:flex-column sm:flex-row items-center justify-between w-full max-w-[700px] rounded-2xl ${style('bg', appConfig.theme.colors.neutrals[700])}`}
         >
           {/* Formulário */}
           <Box
             as="form"
-            styleSheet={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-              width: { xs: '100%', sm: '50%' },
-              textAlign: 'center',
-              marginBottom: '32px',
-            }}
+            className={`mb-[32px] flex flex-col items-center justify-center xs:w-full sm:w-1/2 text-center`}
           >
             <Title tag="h2">Boas vindas de volta!</Title>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+            <Text variant="body3" className={`mb-[32px] ${style('text', appConfig.theme.colors.neutrals[300])}`}>
               {appConfig.name}
             </Text>
 
@@ -65,35 +59,15 @@ const HomePage = () => {
 
           {/* Photo Area */}
           <Box
-            styleSheet={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              maxWidth: '200px',
-              padding: '16px',
-              backgroundColor: appConfig.theme.colors.neutrals[800],
-              border: '1px solid',
-              borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: '10px',
-              flex: 1,
-              minHeight: '240px',
-            }}
+            className={`flex-1 min-h-[240px] rounded-[10px] ${style('border', appConfig.theme.colors.neutrals[999])} border border-solid flex flex-col items-center max-w-[200px] p-[16px] ${style('bg', appConfig.theme.colors.neutrals[800])}]`}
           >
             <Image
-              styleSheet={{
-                borderRadius: '50%',
-                marginBottom: '16px',
-              }}
+              className='rounded-[50%] mb-[16px]'
               src={`https://github.com/${username}.png`}
             />
             <Text
               variant="body4"
-              styleSheet={{
-                color: appConfig.theme.colors.neutrals[200],
-                backgroundColor: appConfig.theme.colors.neutrals[900],
-                padding: '3px 10px',
-                borderRadius: '1000px',
-              }}
+              className={`py-[3px] px-[10px] rounded-[1000px] ${style('bg', appConfig.theme.colors.neutrals[900])} ${style('text', appConfig.theme.colors.neutrals[200])}`}
             >
               {username}
             </Text>
