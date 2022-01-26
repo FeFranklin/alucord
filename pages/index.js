3a43aef3f4e7dd083eb515611bb6f735d907a8e7
Felipe Franklin <felipelfbezerra54@gmail.com>
2022-01-26 18:44:30 +0100

    tailwind and eslint

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const HomePage = () => {
-    return <div>Welcome to nextjs</div>
-};
-
-export default HomePage;
---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'tailwindcss/tailwind.css';
+
+function HomePage() {
+  return <h1 className="text-3xl font-bold underline text-red-600">Welcome to nextjs</h1>;
+}
+
+export default HomePage;
